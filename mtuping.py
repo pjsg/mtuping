b76b7e2c35a3ee7a84e0d74d3e03b4573fc34342
Philip Gladstome <philip@gladstonefamily.net>
2015-01-29 08:45:59 -0500

    Added small ping test

--- mtuping.py
+++ mtuping.py
@@ -12,6 +12,15 @@ conf.debug_match = 1
 ip = IPv6(dst=dest)
 dstaddr = inet_ntop(socket.AF_INET6, ip.dst.net)
 
+debug.recv[:] = []
+
+z3 = sr1(ip/ICMPv6EchoRequest(data='Q' * 1200), verbose=0, timeout=1)
+
+if z3:
+	print "Ping size 1200 returned"
+else:
+	print "Small ping failed"
+
 z = sr1(ip/ICMPv6EchoRequest(data='P' * 1400), verbose=0, timeout=1)
 
 if not z:
@@ -38,8 +47,18 @@ if z1:
 	print "PTB seems to have been ignored"
 else:
 	# Search the packet chain for the ICMP
+	print "No complete packet returned"
 	for resp in debug.recv:
 	    if IPv6 in resp:
 		v6 = resp[IPv6]
 		if v6.src == dstaddr:
 			print "Packet length ", len(str(resp)), ": ", resp.summary()
+
+debug.recv[:] = []
+
+z2 = sr1(ip/ICMPv6EchoRequest(data='Q' * 1200), verbose=0, timeout=1)
+
+if z2:
+	print "Ping size 1200 returned"
+else:
+	print "Small ping failed"
