c368c9d6bf74bda79ace8d49e5d551d4e698443a
Philip Gladstome <philip@gladstonefamily.net>
2015-02-06 09:51:28 -0500

    Add probe of MSS

--- mtuping.py
+++ mtuping.py
@@ -16,12 +16,14 @@ import re
 from collections import OrderedDict
 
 explanations = OrderedDict([
-		( 'NN00N', 'Unreachable.' ),
-		( '.Y22Y', 'Unable to test. Returns fragments without prompting.' ),
-		( '.Y00Y', 'Unable to test. Doesn\'t respond to large echo requests.' ),
-		( '.YY0Y', 'Site does not generate fragmented echo replies. Does change behavior after receiving PTB.' ),
-		( '..Y2.', 'Site generated fragmented echo reply after receving PTB. Success!' ),
-		( '.YYYY', 'Site ignored PTB message. This will cause problems.' ),
+		( '.:.:S:.:.:.', 'Site uses small TCP MSS. Probably have few problems.' ),
+		( 'N:N:N:0:0:N', 'Unreachable.' ),
+		( '.:Y:.:2:2:Y', 'Unable to test. Returns fragments without prompting.' ),
+		( '.:Y:.:0:0:Y', 'Unable to test. Doesn\'t respond to large echo requests.' ),
+		( '.:Y:.:Y:0:Y', 'Site does not generate fragmented echo replies. Does change behavior after receiving PTB.' ),
+		( '.:.:.:Y:2:.', 'Site generated fragmented echo reply after receving PTB. Success!' ),
+		( '.:Y:L:Y:Y:Y', 'Site ignored PTB message and uses large MSS. This will cause problems.' ),
+		( '.:Y:N:Y:Y:Y', 'Site ignored PTB message, but did not respond to SYN. This may cause problems.' ),
 	])
 
 def print_matching(dstaddr, packets, verbose=False):
@@ -30,6 +32,7 @@ def print_matching(dstaddr, packets, verbose=False):
 	    if IPv6 in resp:
 		v6 = resp[IPv6]
 		if v6.src == dstaddr:
+		    if TCP not in resp:
 		        result = result + 1
 			if verbose:
 			    print "Packet length ", len(str(resp)), ": ", resp.summary()
@@ -56,18 +59,18 @@ def doping(args):
 	if verbose:
 		print "Testing ", dstaddr
 
-	status = ''
+	status = []
 
 	z3 = sr1(ip/ICMPv6EchoRequest(data='Q' * 16, id=id, seq=seq), verbose=0, timeout=1)
 	seq = seq + 1
 	if z3:
 		if verbose:
 		    print "Ping size 16 returned"
-		status = status + 'Y'
+		status.append('Y')
 	else:
 		if verbose:
 		    print "Very small ping failed"
-		status = status + 'N'
+		status.append('N')
 
 
 	z3 = sr1(ip/ICMPv6EchoRequest(data='Q' * 1200, id=id, seq=seq), verbose=0, timeout=1)
@@ -76,11 +79,30 @@ def doping(args):
 	if z3:
 		if verbose:
 		    print "Ping size 1200 returned"
-		status = status + 'Y'
+		status.append('Y')
 	else:
 		if verbose:
 		    print "Small ping failed"
-		status = status + 'N'
+		status.append('N')
+
+	# See if we can figure out the MSS
+	z = sr1(ip/TCP(dport=80, sport = random.randint(5555,11111), options=[('MSS', 1440)]), verbose=0, timeout=1)
+
+	if not z:
+		if verbose:
+		    print "No TCP response"
+		status.append('N')
+
+	else:
+	        options = dict(z[TCP].options)
+		mss = options.get('MSS')
+		if mss > 1220:
+		    status.append('L')
+		else:
+		    status.append('S')
+		if verbose:
+		    print "TCP Packet returned. mss=%s" % (mss), repr(z)
+
 
 	z = sr1(ip/ICMPv6EchoRequest(data='P' * 1400, id=id, seq=seq), verbose=0, timeout=1)
 	seq += 1
@@ -88,11 +110,11 @@ def doping(args):
 	if not z:
 		if verbose:
 		    print "No response"
-		status = status + print_matching(dstaddr, debug.recv, verbose)
+		status.append(print_matching(dstaddr, debug.recv, verbose))
 
 		send(ip/ICMPv6PacketTooBig(mtu=mtu)/IPv6(src=dest), verbose=0)
 	else:
-		status = status + 'Y'
+		status.append('Y')
 		if verbose:
 		    print "Packet size 1400 returned. Sending PTB"
 		# Now send the PTB
@@ -108,14 +130,14 @@ def doping(args):
 	seq += 1
 
 	if z1:
-		status = status + 'Y'
+		status.append('Y')
 		if verbose:
 		    print "PTB seems to have been ignored"
 	else:
 		# Search the packet chain for the ICMP
 		if verbose:
 		    print "No complete packet returned"
-		status = status + print_matching(dstaddr, debug.recv, verbose)
+		status.append(print_matching(dstaddr, debug.recv, verbose))
 
 	debug.recv[:] = []
 
@@ -123,17 +145,18 @@ def doping(args):
 	seq += 1
 
 	if z2:
-		status = status + 'Y'
+		status.append('Y')
 		if verbose:
 		    print "Ping size 1200 returned"
 	else:
-		status = status + 'N'
+		status.append('N')
 		if verbose:
 		    print "Small ping failed"
 
 	matched = False
+	statusstring = ":".join([str(x) for x in status])
 	for pattern in explanations:
-		if re.match(pattern, status):
+		if re.match(pattern, statusstring):
 			print "%s: %s" % (dstaddr, explanations[pattern])
 			matched = True
 			break
